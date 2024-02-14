b5f818c8ed216e069dbe3cd15b4b53952a0d24de
jpzomer <jpzm21@gmail.com>
2024-02-14 12:49:39 -0300

    deleted running in files

--- tests/plotWaveform.py
+++
@@ -1,8 +0,0 @@
-from source.waveformConversion import Waveform
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-wvf = Waveform.read_labview_waveform("D:\Dados - Thaler\Documentos\Amaciamento\RunningIn_DatabaseFunc\corr2511.dat",0)
-
-sns.relplot(kind="line", x=wvf.time, y=wvf.data)
-plt.show()
